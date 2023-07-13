d44d92fe5f2d60c1c5bfaecf93614a0ea37df02d
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2023-07-13 17:04:59 +0100

    Background cycles colours

--- main.js
+++ main.js
@@ -12,6 +12,54 @@ function newNumber(prevNumbers,highestNumber){
     }
 }
 
-prevNumbers = []
-highestNumber = parseInt(prompt("How many numbers?"))
+function update(){
+    document.bgColor = "#" + RGBToHex(red,green,blue)
+
+    if(red == 255){
+        if(blue == 0){
+            green++
+        } else {
+            blue--
+        }
+    }
+    if(green == 255){
+        if(red == 0){
+            blue++
+        } else {
+            red--
+        }
+    }
+    if(blue == 255){
+        if(green == 0){
+            red++
+        } else {
+            green--
+        }
+    }
+    red = map(255,0,red)
+    green = map(255,0,green)
+    blue = map(255,0,blue)
+
+    console.log(red,green,blue)
+
+    requestAnimationFrame(update)
+}
+
+function map(highest,lowest,value){
+    if(value > highest){
+        value = highest
+    } else if(value < lowest){
+        value = lowest
+    }
+    return value
+}
+
+const RGBToHex = (r, g, b) => ((r << 16) + (g << 8) + b).toString(16).padStart(6, '0');
+
+var red = 255
+var green = 0
+var blue = 0
+var prevNumbers = []
+var highestNumber = parseInt(prompt("How many numbers?"))
+requestAnimationFrame(update)
 newNumber(prevNumbers,highestNumber)
